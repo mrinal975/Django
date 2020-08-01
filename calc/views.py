4e6da0324c613333619424daa96c9e3361fb65ee
mrinal975 <mrinalmallik1@gmail.com>
2020-08-02 00:21:22 +0600

    get summation result

--- calc/views.py
+++ calc/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpRequest
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {'name':'mrinal'})
+
+def add(request):
+    first = int(request.GET['first'])
+    second = int(request.GET['second'])
+    result = first + second
+    return render(request, 'result.html', {'result':result})
