8248a1d4621caae5f24b9d878d6beac2a89780a5
mrinal975 <mrinalmallik1@gmail.com>
2020-08-03 18:45:45 +0600

    dynamic file image upload

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class designation():
+    id : int
+    name : str
+    img : str
+    desc : str
+    price : int
+
+
--- travello/views.py
+++ travello/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import designation
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    des1 = designation()
+    des1.name = "Dhaka"
+    des1.price = 300
+    des1.img = "destination/1.png"
+    des2 = designation()
+
+    des2.name = "Rajshahi"
+    des2.price = 200
+    des2.img = "destination/2.png"
+    des3 = designation()
+
+    des3.name = "Comilla"
+    des3.price = 210
+    des3.img = "destination/2.png"
+    dess = [des1, des2, des3]
+    return render(request, 'index.html', { 'dess':dess } )
